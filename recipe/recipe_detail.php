1fee75bb6549b97d3d9f4afb5f921c3db9f6c7f2
masa3889 <72597756+masa3889@users.noreply.github.com>
2021-12-17 10:56:37 +0900

    レシピ詳細を実数値で表示 (#14)

--- recipe/recipe_detail.php
+++ recipe/recipe_detail.php
@@ -1,32 +1,32 @@
 <?php
-    /*
+    // 送られてきたrecipeIdを受け取る
+    $recipeId = $_GET['recipeId'];
     // Productオブジェクトを生成する
     require_once __DIR__ . '/../classes/product.php';
     $product = new Product();
     // 選択された商品を取り出す
-    $item = $product->getItem($ident);
-    */
+    $recipe = $product->getRecipe($recipeId);
+    $materials = $product->getMaterials($recipeId);
+    $instructions = $product->getInstructions($recipeId);
     require_once __DIR__ . '/../header.php';
 ?>
 <!-- <h2>レシピ詳細</h2><br> -->
 <table class="recipe">
     <tr>
-    <!-- <th><?= $item['title'] ?></th> -->
-    <th colspan="2" class="recipetitle">トマトサラダ</th>
+        <th colspan="2" class="recipetitle"><?= $recipe['title'] ?></th>
     </tr>
     <tr>
-    <!-- <td><?= $item['comment'] ?></td> -->
-    <td colspan="2">トマトの彩り鮮やかなサラダです。</td>
+        <td colspan="2"><?= $recipe['comment'] ?></td>
     </tr>
     <tr>
-    <th>投稿者: 母</th><td>2人分<td>
+        <th>投稿者: <?= $recipe['poster'] ?></th>
+        <td><?= $recipe['servings'] ?>人分<td>
     <tr>
 </table>
 
 <div class="wrap">
     <section class="r_left">
-        <img class="detail_img" src="https://1.bp.blogspot.com/-M28GZaegp38/X68afYkxWXI/AAAAAAABcQw/tSAxXfPaPp8GtBwzh9eieDieqWXxS47IgCNcBGAsYHQ/s757/food_thai_somtom_salad.png"
-            >
+        <img src="../images/<?= $recipe['imageName'] ?>">
     </section>
     <section class="r_right">
         <table class="recipe recipe_material" id="tableMaterial">
@@ -34,54 +34,44 @@
                 <tr><th>材料</th><th>分量</th></tr>
             </thead>
             <tbody>
+<?php
+            foreach($materials as $material) {
+?>
                 <tr>
-                    <!--
-                    <td><?= $item['material'] ?></td>
-                    <td><?= $item['quantity'] ?></td>
-                    -->
-                    <td class="td_material">トマト</td>
-                    <td class="td_material">2個</td>
-                </tr>
-                <tr>
-                    <td>レタス</td>
-                    <td>3枚</td>
-                </tr>
-                <tr>
-                    <td>きゅうり</td>
-                    <td>1本</td>
-                </tr>
-                <tr>
-                    <td>胡麻ドレッシング</td>
-                    <td>適量</td>
-                </tr>
+                    <td class="td_material"><?= $material['material'] ?></td>
+                    <td class="td_material"><?= $material['quantity'] ?></td> 
+                </tr> 
+<?php
+            }
+?>        
             </tbody>
         </table>
     </section>
 </div>
 <table class="recipe" id="tableInstruction">
     <thead>
-        <tr><th class="td_left">手順</th></tr>
+        <tr><th class="td_left">手順</th><th></th></tr>
     </thead>
     <tbody>
+<?php
+    foreach($instructions as $instruction) {
+?>        
         <tr>
-            <!-- <td><?= $item['instruction'] ?></td> -->
-            <td class="td_left">トマトはくし形切り、きゅうりは薄く斜めに切る。</td>
+            <td><?= $instruction['instructionNumber'] ?>.</td>
+            <td class="td_left"><?= $instruction['instruction'] ?></td>
         </tr>
-        <tr>
-            <td class="td_left">レタスは手で口に入るくらいの大きさにちぎる。</td>
-        </tr>
-        <tr>
-            <td class="td_left">器に盛って、満足するだけのドレッシングをかけて完成。</td>
-        </tr>
+<?php
+    }
+?>
     </tbody>
 </table>
 <div class="wrap2">
-    <a href="./recipe_edit.php?recipeId=<?= $item['recipeId'] ?>">
+    <a href="./recipe_edit.php?recipeId=<?= $recipeId ?>">
         <input type="button" value="編集" class="button_edit"></a>
-    <a href="./recipe_delete.php?recipeId=<?= $item['recipeId'] ?>">
+    <a href="./recipe_delete.php?recipeId=<?= $recipeId ?>">
         <input type="button" value="削除" class="button_delete"></a>
     <br><br>
-    <a href="//localhost/FF/family_foods/recipe_list.php"><input type="button" value="戻る" style="width:180px;height:30px"></a>
+    <a href="../family_foods/recipe_list.php"><input type="button" value="戻る" style="width:180px;height:30px"></a>
 </div>
 </form>
 <?php

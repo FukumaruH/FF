8021455e1de132e7948bf4d24d86a12486aba09d
FukumaruH <kd1304620@st.kobedenshi.ac.jp>
2021-12-17 09:39:27 +0900

    投稿レシピ一覧を実数値で表示

--- family_foods/recipe_list.php
+++ family_foods/recipe_list.php
@@ -1,8 +1,23 @@
 <?php
 require_once __DIR__ . "/../header.php";
+
+//オブジェクトを生成する
+require_once __DIR__ . "/../classes/product.php";
+$recipelist = new Product();
+
+//ビューrecipelistからログインしたユーザーの投稿レシピ一覧情報を取得
+$recipes = $recipelist->getrecipelist($_SESSION['userId']);
+
+echo '<p>こんにちは、'.$_SESSION['userId'].'様。</p>';
+if(empty($recipes)){
+    //投稿レシピの情報がない場合
+    echo '<p>' . $_SESSION['userId'] . '様の投稿レシピ一覧はございません。</p>';
+  } else{
+    //投稿レシピの情報がある場合
+    echo '<p>投稿レシピ一覧は次の通りです。</p>';
+    echo '<p>ご家庭の味ををお楽しみください。</p>';
+  }
 ?>
-<p>こんにちは、<?= $_SESSION['userId'] ?>様。</p>
-<p>ご家庭の味ををお楽しみください。</p>
 
 <p style="text-align: right;margin-right:30px";><a href="//localhost/FF/user/logout.php">
     <input type="button" value="ログアウト" style="width:100px;height:30px"></a></p>
@@ -11,58 +26,37 @@ require_once __DIR__ . "/../header.php";
 
 <h1>投稿レシピ一覧</h1><hr><br>
 
+<?php
+    //ログインしたユーザーの投稿レシピ一覧を表示する
+    $recipeId = 0;
+    foreach($recipes as $recipe){
+        if(isset($recipe['recipeId']) != $recipeId){
+            //recipeIdが変わったら仕切りする
+            echo '<hr width="500"><hr width="500">';
+            $recipeId = $recipe['recipeId'];
+        }
+?>
+<!-- データベースから取得したデータを画面に表示する -->
 <body>
 <div class="wrap">
     <section class="sachi_left">
         <div class="sachi_text">
-            <a href="//localhost/FF/recipe/recipe_detail.php"><h3>トマトサラダ</h3></a>
-            トマトの色どり鮮やかなサ....<br><br>
-            <font color="gray">2021年 11月 29日 11:29</font><br>
+            <a href="//localhost/FF/recipe/recipe_detail.php?recipeId=<?= $recipeId ?>"><h3><?= $recipe['title'] ?></h3></a>
+            <?= $recipe['comment'] ?><br><br>
+            <font color="gray"><?= $recipe['posttime'] ?></font><br>
         </div>
     </section>
     <section class="sachi_right">
         <div class="sachi_img">
             <a href="//localhost/FF/recipe/recipe_detail.php">
-            <img src="https://1.bp.blogspot.com/-M28GZaegp38/X68afYkxWXI/AAAAAAABcQw/tSAxXfPaPp8GtBwzh9eieDieqWXxS47IgCNcBGAsYHQ/s757/food_thai_somtom_salad.png"
+            <img src="../images/<?= $recipe['imageName'] ?>"
             ></a>
         </div>
     </section>
 </div>
 </body>
+<?php
+    }
 
-<hr width="500"><hr width="500">
-
-<body>
-<div class="wrap">
-    <section class="sachi_left">
-        <div class="sachi_text">
-            <!--<a href="#">--><h3>角煮</h3></a>
-            良く煮込まれた柔らかい角....<br><br>
-            <font color="gray">2021年 11月 26日 20:40</font><br>
-        </div>
-    </section>
-    <section class="sachi_right">
-        <div class="sachi_img">
-            <!--<a href="#">-->
-            <img src="https://1.bp.blogspot.com/-m37XlCVf2QM/XzIec2XVlBI/AAAAAAABajM/97kToy2rmAs_JPxWaQaYCDA1AQkrxIBrgCNcBGAsYHQ/s400/food_rendang_rundan.png"
-            ></a>
-        </div>
-    </section>
-</div>
-</body>
-
-
-<!-- <body>
-<a href="//localhost/FF/family_foods/recipe_sample/salada.php"><h3>トマトサラダ</h3></a>
-トマトの色どり鮮やかなサ....<br><br>
-<font color="gray">2021年 11月 29日 11:29</font><br>
-
-<div class="example">
-<a href="//localhost/FF/family_foods/recipe_sample/salada.php">
-<p><img src="https://1.bp.blogspot.com/-M28GZaegp38/X68afYkxWXI/AAAAAAABcQw/tSAxXfPaPp8GtBwzh9eieDieqWXxS47IgCNcBGAsYHQ/s757/food_thai_somtom_salad.png"
-alt="画像のサンプル" width="190" height="160"></p></a>
-</div></body> -->
-
-<?php 
     require_once __DIR__ . "/../footer.php";
 ?>

74365530049beab84480ce4226c7bab0627ebf24
FukumaruH <kd1304620@st.kobedenshi.ac.jp>
2022-01-20 13:05:04 +0900

    コメント関係の細かい修正をしました

--- classes/cart.php
+++ classes/cart.php
@@ -3,24 +3,6 @@
   require_once __DIR__ . '/dbdata.php';
 
   class Cart extends DbData{
-    // 商品をカートに入れる ・・ テーブルcartに登録する
-    public function addItem($userId, $ident, $quantity){
-      // すでにカート内にその商品がはいっているかどうかをチェックする
-      $sql = "select * from cart where userId = ? and ident = ?";
-      $stmt = $this->query($sql, [$userId, $ident]);
-      $cart_item = $stmt->fetch();
-      if($cart_item){
-        // カート内にすでに入っているので、今回の注文数を追加する
-        $new_quantity = $quantity + $cart_item['quantity'];
-        if($new_quantity > 10 ) $new_quantity = 10;
-        $sql = "update cart set quantity = ? where userId = ? and ident = ?";
-        $result = $this->exec($sql, [$new_quantity, $userId, $ident]);
-      } else {
-        // カート内にはまだ入っていないので登録する
-        $sql = "insert into cart values(?, ?, ?)";
-        $result = $this->exec($sql, [$userId, $ident, $quantity]);
-      }
-    }
 
     //各テーブルの指定されたrecipeIdを持つ情報を削除する
     public function deleterecipe($recipeId){
@@ -30,25 +12,9 @@
       $result = $this->exec($sql, [$recipeId]);
       $sql = "delete from materials where recipeId = ?";
       $result = $this->exec($sql, [$recipeId]);
+      $sql = "delete from comments where recipeId = ?";
+      $result = $this->exec($sql, [$recipeId]);
       $sql = "delete from recipes where recipeId = ?";
-      $result = $this->exec($sql, [$recipeId]);
-    }
-
-    //イメージテーブルの指定されたrecipeIdを持つ情報を削除する
-    public function deleteimage($recipeId){
-      $sql = "delete from images where recipeId = ?";
-      $result = $this->exec($sql, [$recipeId]);
-    }
-
-    //手順テーブルの指定されたrecipeIdを持つ情報を削除する
-    public function deleteinstruction($recipeId){
-      $sql = "delete from instructions where recipeId = ?";
-      $result = $this->exec($sql, [$recipeId]);
-    }
-
-    //材料テーブルの指定されたrecipeIdを持つ情報を削除する
-    public function deletematerial($recipeId){
-      $sql = "delete from materials where recipeId = ?";
       $result = $this->exec($sql, [$recipeId]);
     }
 
@@ -58,20 +24,4 @@
       $result = $this->exec($sql, [$title, $poster, $comment, $servings, date("Y-m-d H:i:s"), $recipeId]);
     }
 
-    // カート内の商品を削除する
-    public function deleteItem($userId, $ident){
-      $sql = "delete from cart where userId = ? and ident = ?";
-      $result = $this->exec($sql, [$userId, $ident]);
-    }
-
-    //ゲストからログインした場合、カート内の仮のユーザーIDをログイン後の正式なユーザーIDの値に変更する
-    public function changeUserId($tempId, $userId){
-      $sql = "select * from cart where userId = ?";
-      $stmt = $this->query($sql, [$tempId]);
-      $cart_items = $stmt->fetchAll();
-      foreach($cart_items as $item){
-        $this->addItem($userId, $item['ident'], $item['quantity']);
-        $this->deleteItem($tempId, $item['ident']);
-      }
-    }
   }
--- classes/user.php
+++ classes/user.php
@@ -9,14 +9,6 @@
             $stmt = $this->query($sql, [$userId, $password]);
             return $stmt->fetch();
         }
-
-        //ゲストからログインした場合、カート内の商品が入っていれば、ログイン後のユーザーID(メールアドレス)に変更する
-        public function changeCartUserId($tempId, $userId){
-            //Cartオブジェクトを生成し、カート内の商品のユーザーIDに変更する
-            require_once __DIR__ ."/../classes/cart.php";
-            $cart = new Cart();
-            $cart->changeUserId($tempId, $userId);
-        }
     
         //ユーザー登録処理
         public function signUp($userId, $password){
@@ -40,29 +32,4 @@
             }
         }
 
-        //ユーザー情報更新処理
-    public function updateUser($userId, $userName, $kana, $zip, $address, $tel, $password, $tempId){
-        $sql = "update users set userId = ?, userName = ?, kana = ?, zip = ?, address = ?, tel = ?, password = ? where userId = ?";
-        $result = $this->exec($sql, [$userId, $userName, $kana, $zip, $address, $tel, $password, $tempId]);
-
-        if($result){
-            //更新に成功したが、Cart内に仮のユーザーIDの商品が入っていた場合、新しんユーザーIDに置き換える
-            //また、過去の注文履歴のユーザーIDもあたしいユーザーIDに置き換える
-            if($userId !== $tempId){
-                $this->changeCartUserId($tempId, $userId);
-                $this->changeOrderHistoryUserId($tempId, $userId);
-            }
-            return '';
-        } else{
-            return 'ユーザー情報の更新ができませんでした。管理者にお問い合わせください。';
-        }
-    }
-
-    //ユーザーID(Eメール)を変更した場合、過去の注文履歴のユーザーID(Eメール)を新しいユーザーIDに変更する
-    public function changeOrderHistoryUserId($tempId, $userId){
-        //Orderオブジェクトを生成し、注文履歴のユーザーID(Eメール)を変更する
-        require_once __DIR__ . "/../classes/order.php";
-        $order = new Order();
-        $order->changeUserId($tempId, $userId);
-    }
     }

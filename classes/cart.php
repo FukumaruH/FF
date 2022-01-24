05590a816ef2d2ce53ad88c1494b5b3ef9a64a13
FukumaruH <kd1304620@st.kobedenshi.ac.jp>
2022-01-24 13:25:42 +0900

    細かい修正

--- classes/cart.php
+++ classes/cart.php
@@ -18,6 +18,24 @@
       $result = $this->exec($sql, [$recipeId]);
     }
 
+    //イメージテーブルの指定されたrecipeIdを持つ情報を削除する
+    public function deleteimage($recipeId){
+      $sql = "delete from images where recipeId = ?";
+      $result = $this->exec($sql, [$recipeId]);
+    }
+
+    //手順テーブルの指定されたrecipeIdを持つ情報を削除する
+    public function deleteinstruction($recipeId){
+      $sql = "delete from instructions where recipeId = ?";
+      $result = $this->exec($sql, [$recipeId]);
+    }
+
+    //材料テーブルの指定されたrecipeIdを持つ情報を削除する
+    public function deletematerial($recipeId){
+      $sql = "delete from materials where recipeId = ?";
+      $result = $this->exec($sql, [$recipeId]);
+    }
+
     //レシピテーブルを更新する
     public function updaterecipe($title, $poster, $comment, $servings, $posttime, $recipeId){
       $sql = "update recipes set title = ?, poster = ?, comment = ?, servings = ?, posttime = ? where recipeId = ?";
